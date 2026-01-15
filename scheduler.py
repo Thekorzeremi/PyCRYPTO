251cf8034e69ca71ed592b36362b2a02cfc0cc5c
Thekorzeremi <kremi02@outlook.fr>
2026-01-15 16:01:05 +0100

    log

--- scheduler.py
+++ scheduler.py
@@ -3,12 +3,11 @@ from update_data import run_pipeline
 
 scheduler = BackgroundScheduler()
 
-def start_scheduler():
+def start_scheduler(scrapper: int):
     """
     Démarre le scheduler APScheduler pour exécuter le pipeline automatiquement.
     """
-    scheduler.add_job(run_pipeline, 'interval', minutes=10, id="crypto_scraping")
-
+    scheduler.add_job(run_pipeline, 'interval', minutes=10, id="crypto_scraping", args=[scrapper])
     scheduler.start()
     print("Scheduler lancé : scraping automatique toutes les 10 minutes")
 
